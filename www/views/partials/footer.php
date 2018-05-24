22ac8b7a1135a28c6ee96ad1f97dd94fad2527a0
Kristof Buts <github@kristofbuts.be>
2018-05-24 14:11:21 +0200

    Header and footer

---
+++ www/views/partials/footer.php
@@ -0,0 +1,30 @@
+<hr>
+
+<footer class="container">
+		<span class="float-right">
+				<p>&copy; <?= date('Y'); ?> KLJ Wiekevorst <br>
+				webdesign by <a href="https://kristofbuts.be" target="_blank">Kristof Buts</a></p>
+			</span>
+</footer>
+
+<script src="static/js/vendor/modernizr-3.5.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="/static/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="/static/js/vendor/popper.min.js"></script>
+<script src="/static/js/vendor/bootstrap.min.js"></script>
+
+<script src="/static/js/plugins.js"></script>
+
+<script src="/static/js/jquery.magnific-popup.min.js"></script>
+
+<script>
+	// $(document).ready(function () {
+	// 	$('.mfp-link').magnificPopup({
+	// 		type: 'image',
+	// 		gallery: {
+	// 			enabled: true,
+	// 			navigateByImgClick: true,
+	// 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	// 		}
+	// 	})
+	// });
+</script>
--- www/views/partials/head.php
+++ www/views/partials/head.php
@@ -1,2 +1,26 @@
-<!-- TITLE -->
-<title><?=$this->e($title)?></title>
+	<!-- META -->
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="KLJ Wiekevorst is een bruisende jeugdbeweging voor jongeren van 5 tot 35 jaar.">
+	<meta name="author" content="Kristof Buts">
+
+	<!-- TITLE -->
+	<title><?=$this->e($title)?></title>
+
+	<!-- FAVICONS -->
+
+	<!-- STYLESHEETS AND LIBRARIES -->
+	<link rel="stylesheet" href="/static/css/normalize.min.css">
+	<link rel="stylesheet" href="/static/css/bootstrap.min.css">
+<!--	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans|Open+Sans" rel="stylesheet">-->
+	<link rel="stylesheet" href="/static/css/main.min.css">
+	<link rel="stylesheet" href="/static/css/custom.css">
+	<?php
+	if ($this->e($extracss)) { ?>
+		<link rel="stylesheet" href="<?=$this->e($extracss)?>">
+	<?php }
+	?>
+
+	<link rel="stylesheet" href="/static/css/magnific-popup.css">
+
