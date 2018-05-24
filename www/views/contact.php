f86281e5502c1038bca97ab89793dea65ae17941
Kristof Buts <github@kristofbuts.be>
2018-05-24 21:25:44 +0200

    Contact started

--- www/views/contact.php
+++ www/views/contact.php
@@ -6,8 +6,53 @@
  * Time: 21:08
  */
 
-$this->layout('template', ['title' => 'Contact - KLJ Wiekevorst', 'id' => 'contact']) ?>
+$this->layout('template', ['title' => 'Contact - KLJ Wiekevorst', 'id' => 'contact', 'extracss' => '/static/css/contact.css']) ?>
 
 <div class="container">
 	<h1>Contact</h1>
+
+	<div class="row">
+		<div class="col">
+			Het (factuur)adres van onze lokalen is <br>
+			<address class="leftmargin">
+				<strong>KLJ Wiekevorst</strong> <br>
+				Dalstraat 22A <br>
+				2222 Wiekevorst
+			</address>
+		</div><!-- /col -->
+		<div class="col">
+			GOOGLE MAPS
+		</div><!-- /col -->
+	</div><!-- /row -->
+
+	<p>Met algemene vragen of problemen kan je steeds terecht bij onze hoofdleiding.</p>
+
+	<div class="row">
+		<div class="col-md-3 leftmargin">
+			<strong>Kristof Buts</strong> <br>
+			+32 477 43 08 65
+		</div><!-- /col-md-3 -->
+		<div class="col-md-3">
+			<strong>Joppe Van den Sande</strong> <br>
+			+32 477 43 08 65
+		</div><!-- /col-md-3 -->
+	</div><!-- /row -->
+
+	<p>Je kan hen beide bereiken via mail op hoofdleiding@kljwiekevorst.be of langs het contactformulier.</p>
+
+	<p>Heb je post en wil je dat die bekend gemaakt wordt voor heel het bestuur? Dan is het best dat je deze naar onze secretaris verstuurd.</p>
+	<div class="row">
+		<div class="col-md-3 leftmargin">
+			<address>
+				<strong>Michelle Cuylaerts</strong> <br>
+				Wiekevorstse Goorweg 17 <br>
+				2222 Wiekevorst <br>
+				secretaris@kljwiekevorst.be
+			</address>
+		</div><!-- /col-md-3 offset-md-3 -->
+	</div><!-- /row -->
+
+	<p>Heb je post (afdelingszending) en wil je dat die bekend wordt gemaakt voor heel KLJ Wiekevorst? Dan is het best dat je deze post stuurt naar onze secretaris. Anders kunnen we niet garanderen dat iedereen op de hoogte is.
+
+	</p>
 </div>
