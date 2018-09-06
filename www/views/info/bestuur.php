7776cb9b2a15e8d2c9b5dd078e919deac8e9c9d7
Kristof Buts <kr.buts@gmail.com>
2018-09-06 19:18:45 +0200

    Bestuur done

--- www/views/info/bestuur.php
+++ www/views/info/bestuur.php
@@ -11,26 +11,37 @@ $this->layout('template', ['title' => 'Bestuur - KLJ Wiekevorst', 'id' => 'info/
 		<ul>
 			<li>Hoofdleiding: Kristof Buts en Joppe Van den Sande</li>
 			<li>Secretaris: Michelle Cuylaerts</li>
-			<li>Kassier: Lien Gysels</li>
+			<li>Bestuur: Lien Gysels</li>
+			<li>Volwassenbegeleider: Kurt Vets</li>
 			<li>
 				De rest van de leiding
 				<ul>
 					<li>Astrid Bries</li>
 					<li>Viktor Bries</li>
 					<li>Sarah Broeckx</li>
+					<li>Lisa Cools</li>
+					<li>Leticia Cornelis</li>
 					<li>Jef Cuylaerts</li>
 					<li>Maaike Daneels</li>
 					<li>Seppe De Winter</li>
+					<li>Jef Doms</li>
 					<li>Volker Goossens</li>
+					<li>Lien Helsen</li>
+					<li>Jelle Hermans</li>
 					<li>Michiel Hoefkens</li>
+					<li>Senne Hoefkens</li>
+					<li>Casimir Horemans</li>
+					<li>Astrid Lambaerts</li>
 					<li>Laura Smits</li>
 					<li>Lander Van den Sande</li>
+					<li>Ben Van Oevelen</li>
 					<li>Jo Vankerckhoven</li>
+					<li>Jeffrey Vermeulen</li>
 					<li>Julie Vermeulen</li>
+					<li>Sander Vermeulen</li>
 				</ul>
 			</li>
 		</ul>
-		<p>Ook de volwassenbegeleider (Kurt Vets) maakt deel uit van ons bestuur.</p>
 		<p>Wil je meer te weten komen over onze leiding? Kijk dan op de <a href="/leiding">leidingspagina</a>!</p>
 		<br />
 		<div class="photos">
