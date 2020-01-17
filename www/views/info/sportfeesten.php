4eb9257cf2c714ce84822b568ba6b81d0beff7eb
Kristof Buts <butskristof@tbMBP13.local>
2020-01-17 12:32:59 +0100

    Add sportfeest pictures

--- www/views/info/sportfeesten.php
+++ www/views/info/sportfeesten.php
@@ -7,29 +7,48 @@ $this->layout('template', ['title' => 'Sportfeesten - KLJ Wiekevorst', 'id' => '
 		Sportfeesten
 	</h1>
 
-	<div class="col-md-10 offset-md-1">
-		<p>Na ons zomerkamp van 11-20 juli stopt KLJ Wiekevorst ook niet. In de maand augustus trekken we 3 à 4 keer naar een sportfeest om ons daar in verschillende disciplines te meten met andere KLJ-afdelingen.
-			<br>
-			De verschillende disciplines zijn dans (kringdans, parendans, vrije ritmiek en bondsreeks), atletiek en trouwtrekken, wimpelen (kleine vlaggen), vendelen (grote vlaggen) en piramides.
-			<br>
-			Een hele dag doen we ons uiterste best om uit te blinken op alle vlakken, maar vooral om een hechte groep te vormen die enorm veel plezier beleeft.
-		</p>
+		<div class="col-md-10 offset-md-1">
+			<p>Na ons zomerkamp van 11-20 juli stopt KLJ Wiekevorst ook niet. In de maand augustus trekken we 3 à 4 keer naar een sportfeest om ons daar in verschillende disciplines te meten met andere KLJ-afdelingen.
+				<br>
+				De verschillende disciplines zijn dans (kringdans, parendans, vrije ritmiek en bondsreeks), atletiek en trouwtrekken, wimpelen (kleine vlaggen), vendelen (grote vlaggen) en piramides.
+				<br>
+				Een hele dag doen we ons uiterste best om uit te blinken op alle vlakken, maar vooral om een hechte groep te vormen die enorm veel plezier beleeft.
+			</p>
 
-		<p>Kalender 2020 <br>
-		<ul>
-			<li>15 augustus 2020: Interregionaal sportfeest Loenhout</li>
-			<li>16 augustus 2020: Regionaal sportfeest Loenhout</li>
-			<li>23 augustus 2020: Provinciaal sportfeest Sint-Lenaerts</li>
-		</ul>
-		</p>
+			<p>Kalender 2020 <br>
+			<ul>
+				<li>15 augustus 2020: Interregionaal sportfeest Loenhout</li>
+				<li>16 augustus 2020: Regionaal sportfeest Loenhout</li>
+				<li>23 augustus 2020: Provinciaal sportfeest Sint-Lenaerts</li>
+			</ul>
+			</p>
 
-		<p>
-			Voor meer info; contacteer één van de sportverantwoordelijken:
+			<p>
+				Voor meer info; contacteer één van de sportverantwoordelijken:
 			<ul>
 				<li>Lander Van den Sande (0471/41.35.58)</li>
 				<li>Julie Vermeulen (0473/36.66.46)</li>
 				<li>Jeffrey Vermeulen (0468/17.61.46‬</li>
 			</ul>
-		</p>
+			</p>
+			<div class="row">
+				<div class="col-md-6">
+					<a class="popup thumb mfp-link" href="/static/img/sportfeest/sportfeest1.jpg">
+						<img src="/static/img/sportfeest/sportfeest1.jpg" alt="Sportfeest KLJ Wiekevorst" class="img-fluid mb-3" />
+					</a>
+					<a class="popup thumb mfp-link" href="/static/img/sportfeest/sportfeest2.jpg">
+						<img src="/static/img/sportfeest/sportfeest2.jpg" alt="Sportfeest KLJ Wiekevorst" class="img-fluid mb-3" />
+					</a>
+				</div>
+				<div class="col-md-6">
+					<a class="popup thumb mfp-link" href="/static/img/sportfeest/sportfeest3.jpg">
+						<img src="/static/img/sportfeest/sportfeest3.jpg" alt="Sportfeest KLJ Wiekevorst" class="img-fluid mb-3" />
+					</a>
+					<a class="popup thumb mfp-link" href="/static/img/sportfeest/sportfeest4.jpg">
+						<img src="/static/img/sportfeest/sportfeest4.jpg" alt="Sportfeest KLJ Wiekevorst" class="img-fluid mb-3" />
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
