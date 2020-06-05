8115e126fff6974859697e758186ebec976a8861
SeppeDeWinter <seppe.dewinter@hotmail.com>
2020-06-05 22:23:05 +0200

    Kamp2020

--- www/views/info/kamp.php
+++ www/views/info/kamp.php
@@ -8,9 +8,24 @@ $this->layout('template', ['title' => 'Kamp - KLJ Wiekevorst', 'id' => 'info/kam
 	</h1>
 
 	<div class="col-md-10 offset-md-1">
-		<p>Elk jaar gaat KLJ Wiekevorst 8 dagen op binnenlands kamp. De kampplaats is elk jaar verschillend maar de periode is  wel altijd ongeveer dezelfde: tussen 11 en 20 juli (bijvoorbeeld: van 12 tot 20 juli). Wil je graag meegaan op kamp, hou dan zeker dan deze periode vrij.</p>
-		<p>Om de vier jaar organiseren we voor de +16 een buitenlands kamp. Zij gaan dan niet mee op binnenlands kamp maar krijgen dan de kans om de kampsfeer in het buitenland op te snuiven.</p>
-		<p>De kampinformatie wordt op het einde van het werkjaar (mei - juni) rondgebracht. Hierin staat alle praktische informatie. Het <a href="/static/files/kampreglement.pdf">kampreglement</a> geldt uiteraard wel op elk kamp.</p>
+		<h2>We mogen op kamp!!</h2>
+		<p>Covid-19 heeft de jeugd niet klein gekregen, we kunnen dit jaar op kamp!</p>
+		<p>Het kamp zal dit jaar doorgaan van <b>12 juli</b> t.e.m. <b>20 juli</b>.</p>
+		<p>Specifieke informatie kan je vinden in de kampkrant (deze mag je binennkort in jullie brievenbus verwachten), ook over de "Corona-maatregelen" (bijvoorbeeld hoe wij de contactbubbels van maximaal 50 personen zullen aanpakkken) zal je hierin meer informatie vinden. Op <b>22 juni</b> organiseren wij een <b>online infoavond</b> voor ouders om de "Corona-maatregelen" iets "persoonlijker" toe te lichten. Informatie hierover zal worden aangekondigd op onze <a href="https://www.facebook.com/kljwiekevorst">facebook pagina</a>. Wij vragen om al uw vragen op voorhand door te sturen naar de <a href="mailto:hoofdleiding@kljwiekevorst.be"> hoofdleiding </a>, dan kan deze avond wat vlotter verlopen!</p>
+		<p>Belangrijke voorwaarde om te kunnen deelnemen aan het kamp is: </p>
+		<ol>
+			<li>Dat je <u>niet</u> ziek mag zijn <b>vijf dagen</b> voor de aanvang van het kamp.</li>
+			<li>Risicogroepen moeten toestemming hebben van hun arts en/of ouders, meer informatie hierover vind je in dit document <a href="/static/files/Risicogroepen.pdf">LIJST PEDIATERS RISICOGROEPEN</a>.</li>
+		</ol>
+		<p>In beide gevallen is het best om bij twijfel contact op te nemen met de arts!</p>
+		
+		<p>Zoals elk jaar gelden er ook algemene regels op kamp, deze vindt je hier:<a href="/static/files/kampreglement.pdf">kampreglement</a>.</p>
+	
+		<p>COMMING SOON! <i>Stuur een online brief naar het kamp</i></p>
+		
+		<p><b>Inschrijven voor het kamp gebeurt dit jaar online, dit kan via onderstaand inschrijvingsformulier:</b></p>
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfA0jM2lS01vi-fUwMBa6PfXjFojWiz5m-OVUYqDD2gKRpRzQ/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 	</div>
 </div>
 </div>
+
