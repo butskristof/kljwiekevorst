4594979cbe142da8a1c6f0eaff4f19f21fa54926
SeppeDeWinter <seppe.dewinter@hotmail.com>
2020-06-24 20:26:59 +0200

    Informatie over nieuwe lokalen toegevoegd aan geschiedenis

--- www/views/info/geschiedenis.php
+++ www/views/info/geschiedenis.php
@@ -11,21 +11,25 @@ $this->layout('template', ['title' => 'Geschiedenis - KLJ Wiekevorst', 'id' => '
 
 	<p>Hieronder staan onze belangrijkste gebeurtenissen vermeld:</p>
 	<ul>
-		<li>1928: oprichting BJB - meisjesafdeling</li>
-		<li>1930: oprichting BJB - jongensafdeling</li>
-		<li>1965: naamsverandering van BJB (boer(innen)jeugdbond) naar KLJ (Katholieke Landelijke Jeugd)</li>
-		<li>1965: activiteiten zijn gemengd</li>
-		<li>1967: eerste Paasbal</li>
-		<li>2001: start bouw huidige lokalen in de Dalstraat</li>
-		<li>2002: viering 75-jarig bestaan KLJ in Vlaanderen</li>
-		<li>2003: inhuldiging nieuwe lokalen + viering 75-jarig bestaan</li>
-		<li>2003: start nieuwe leeftijdsgroep: -12 (vierde en vijfde studiejaar)</li>
-		<li>2004: eerste buitenlands kamp +16 in Zuid-Frankrijk</li>
-		<li>2005: organisatie provinciaal sportfeest</li>
-		<li>2009: organisatie landjuweel</li>
-		<li>2009: start nieuwe leeftijdsgroep: -9 (eerste, tweede en derde studiejaar)</li>
-		<li>2018: feestweekend 90 jaar KLJ Wiekevorst</li>
+		<li>1928: Oprichting BJB - meisjesafdeling</li>
+		<li>1930: Oprichting BJB - jongensafdeling</li>
+		<li>1965: Naamsverandering van BJB (boer(innen)jeugdbond) naar KLJ (Katholieke Landelijke Jeugd)</li>
+		<li>1965: Activiteiten zijn gemengd</li>
+		<li>1967: Eerste Paasbal</li>
+		<li>2001: Start bouw lokalen in de Dalstraat</li>
+		<li>2002: Viering 75-jarig bestaan KLJ in Vlaanderen</li>
+		<li>2003: Inhuldiging nieuwe lokalen + viering 75-jarig bestaan</li>
+		<li>2003: Start nieuwe leeftijdsgroep: -12 (vierde en vijfde studiejaar)</li>
+		<li>2004: Eerste buitenlands kamp +16 in Zuid-Frankrijk</li>
+		<li>2005: Organisatie provinciaal sportfeest</li>
+		<li>2009: Organisatie landjuweel</li>
+		<li>2009: Start nieuwe leeftijdsgroep: -9 (eerste, tweede en derde studiejaar)</li>
+		<li>2018: Feestweekend 90 jaar KLJ Wiekevorst</li>
+		<li>2019: Start bouw extra lokalen in de Dalstraat</li>
+		<li>2020: Nieuwe lokalen in de dalstraat worden voor de eerste keer verhuurd voor zomerkampen</li>
 	</ul>
-	<p>Natuurlijk heeft KLJ Wiekevorst een veel rijkere geschiedenis dan deze data. Het is echter onbegonnen werk om al deze data op te sommen. Denk maar aan de sportfeesten waar reeds al vele prijzen gewonnen zijn en aan alle kampen, evenementen, ...</p></div>
+	<p>Natuurlijk heeft KLJ Wiekevorst een veel rijkere geschiedenis dan deze data. Het is echter onbegonnen werk om al deze data op te sommen. Denk maar aan de sportfeesten waar reeds al vele prijzen gewonnen zijn en aan alle kampen, evenementen, ...</p>
+	<p>Wij trachten een archief bij te houden van documenten, foto's, voorwerpen, ... van KLJ Wiekevorst. Heb je hierover vragen of ben je in het bezit van foto's neem dan zeker contact op!</p>
+	</div>
 </div>
 </div>
