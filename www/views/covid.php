1fd0d6da16507bec9a6037610eb39558428c3fba
SeppeDeWinter <seppe.dewinter@hotmail.com>
2020-09-13 11:17:14 +0200

    update covid maatregelen

--- www/views/covid.php
+++ www/views/covid.php
@@ -7,6 +7,52 @@ $this->layout('template', ['title' => 'Covid-19 Veiligheidsvoorschriften - KLJ W
 
 	<p>Zoals overal, gelden er dit jaar bij KLJ Wiekevorst een aantal Covid-19 veiligheidsvoorschriften zodat we onze activiteiten op een veilige manier kunnen organiseren. Hieronder vind je een kort overzicht van deze maatregelen. Uiteraard kunnen deze door de loop van het jaar nog veranderen. Houd dus regelmatig deze pagina en onze Facebook pagina in het oog voor de laatste informatie en updates.</p>
 
+	<h2>Fase Geel [UPDATE]:</h2>
+
+	<p>Sinds begin september werkt ook het jeugdwerk met kleurcodes. De codes worden op dezelfde manier bepaald als in het onderwijs.</p>
+
+	<p>Momenteel bevinden we ons in: <b>Fase geel</b></p>
+
+	<p>In het kort wil dit zeggen dat:</p>
+
+	<ul>
+		<li>We bubbels moeten vormen van maximaal 50 personen (inclusief leiding.)</li>
+		<li>
+			Voor -9, -12, -14 en +14 <b>leden</b>:
+			<ul>
+				<li><u>Binnen</u> deze bubbel <i>niet</i> verplicht is een mondmasker te dragen of social distance te houden.</li>
+				<li>Hygiëne heel belangrijk blijft.</li>
+				<li>Apart materiaal, lokalen en terreinen zullen worden gebruikt per bubbel.</li>
+			</ul>
+		</li>
+		<li>
+			Voor +16 <b>leden</b>:
+			<ul>
+				<li><u>Binnen</u> deze bubbel <i>niet</i> verplicht is een mondmasker te dragen of social distance te houden.</li>
+				<li>Hygiëne heel belangrijk blijft.</li>
+				<li>Apart materiaal, lokalen en terreinen zullen worden gebruikt per bubbel.</li>
+				<li>Bij het serveren van eten en/of drinken de <a href = "https://www.info-coronavirus.be/nl/faq/#winkels">horecaregels</a> gelden.</li>
+			</ul>
+		</li>
+		<li>
+			Voor +20 <b>leden</b>:
+			<ul>
+				<li><u>Binnen</u> deze bubbel <i>WEL</i> verplicht is social distance te houden of een mondmasker te dragen.</li>
+				<li>Hygiëne heel belangrijk blijft.</li>
+				<li>Apart materiaal, lokalen en terreinen zullen worden gebruikt per bubbel.</li>
+				<li>Bij het serveren van eten en/of drinken de <a href = "https://www.info-coronavirus.be/nl/faq/#winkels">horecaregels</a> gelden.</li>
+			</ul>
+		</li>
+	</ul>
+
+	<h2>Wie mag er meedoen met de activiteiten? [UPDATE]</h2>
+	<p>Iedereen mag naar de activiteiten komen behalve:</p>
+	<ul>
+		<li>Als je ziek bent of ziek bent geweest <u>3 dagen</u> voor de activiteit.</li>
+		<li>Als je in contact bent geweest met een besmet persoon. Dan mag je <u>14 dagen</u> niet deelnemen aan de activiteiten.</li>
+		<li>Als je tot een risicogroep behoort. Dan mag je deelnemen mits toestemming van een arts.</li>
+	</ul>
+
     <h2>Bubbels:</h2>
 
     <p>Onze activiteiten zullen georganiseerd worden in bubbels. Deze bubbels zullen dezelfde zijn als onze leeftijdsgroepen. Dit wil dus zeggen:</p>
@@ -66,8 +112,6 @@ $this->layout('template', ['title' => 'Covid-19 Veiligheidsvoorschriften - KLJ W
 
     <p>Elke bubbel zal ook zijn eigen materiaal, lokalen en terreinen ter beschikking hebben om zo besmettingen verder te voorkomen.</p>
 
-	<p>Heb je post en wil je dat die bekend gemaakt wordt voor heel het bestuur? Dan is het best dat je deze naar onze secretaris verstuurt.</p>
-    
     <h2>Contactopsporing:</h2>
 
     <p>Om contactopsporing mogelijk te maken zullen wij, de eerste keer dat een lid naar een activiteit komt, contactgegevens opvragen. Hierna zullen wij telkens aanduiden wanneer het lid naar de activiteit geweest is. Het formuliertje voor de gegevens moet dus maar één keer worden ingevuld.</p>
