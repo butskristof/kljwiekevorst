b378e1659ae824fb5afef6c43830cd6d67f2a474
SeppeDeWinter <seppe.dewinter@hotmail.com>
2021-01-15 21:37:41 +0100

    added forms restaurantdag

--- www/views/Restodag.php
+++ www/views/Restodag.php
@@ -4,7 +4,9 @@ $this->layout('template', ['title' => 'Restaurant Dag - KLJ Wiekevorst', 'id' =>
 
 <div class="container">
 
-    <h1>Restaurant Dag: BESTELLING</h1>
-    <h1>Formulier komt binnenkort</h1>
+    <h1>Restaurant Dag: Bestelling</h1>
+    
+    <iframe src="https://docs.google.com/forms/d/1nrmPsXKdcVV4NE1MgpOlreQIY7mXqP2OcdwHtePOKu0/viewform?embedded=true" name="Restaurantdag" width="600px" height="2000px" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" style="border:0px #FFFFFF none; float=center; margin=0px; padding=0px;" allowfullscreen></iframe>
+
     
  </div>
