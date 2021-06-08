7bd86b15720bcdae710d9fcbef5dc789080cec11
SeppeDeWinter <seppe.dewinter@hotmail.com>
2021-06-08 18:17:08 +0200

    covid update

--- www/views/covid.php
+++ www/views/covid.php
@@ -7,18 +7,14 @@ $this->layout('template', ['title' => 'Covid-19 Veiligheidsvoorschriften - KLJ W
 
 	<p>Zoals overal, gelden er dit jaar bij KLJ Wiekevorst een aantal Covid-19 veiligheidsvoorschriften zodat we onze activiteiten op een veilige manier kunnen organiseren. Hieronder vind je een kort overzicht van deze maatregelen. Uiteraard kunnen deze door de loop van het jaar nog veranderen. Houd dus regelmatig deze pagina en onze Facebook pagina in het oog voor de laatste informatie en updates.</p>
 
-	<h2>Nieuwe maatregelen sinds 1 februari 2021</h2>
+	<h2>Nieuwe maatregelen vanaf 9 juni 2021</h2>
 
 	<ul>
-		<li>Activiteiten mogen opnieuw doorgaan voor alle kinderen jonger dan 18 jaar.</li> 
-		<li>Activiteiten zullen buiten doorgaan (aangeraden voor kinderen jonger dan 12 jaar en verplicht voor jongeren ouder dan 12 jaar).</li>
-		<li>Mondmasker: verplicht voor alle leiding en kinderen ouder dan 12 jaar, niet verplicth voor kinderen jonger dan 12 jaar.</li>
-		<li>Groepjes van maximaal 10 kinderen (leiding niet inbegrepen).</li>
+		<li>Activiteiten binnen en buiten mogen in groepen van maximaal 50 personen, dit geldt voor alle leeftijden</li>
+		<li>Vanaf de leeftijd van 12 jaar dragen de leden een mondmasker</li>
+		<li>basis hygiene: handen wassen, papieren zakdoek gebruiken, afstand houden, ...</li>
 	</ul>
-
-	<h2>Praktisch</h2>
-	<p>We zullen week om week afwisselen activiteiten doen met kinderen jonger dan 12 jaar en kinderen ouder dan 12 jaar. Dus de ene week is het activiteit voor de -9 en -12 en de andere week is het activiteit voor de -14 en +14.</p>
-	<p>Om de groepjes van 10 praktisch te organiseren vragen we om jouw kind op voorhand in te schrijven voor de activiteit (inschrijving moet ten laatste binnen zijn op zaterdag middag voor de activteit op zondag). Je zal op zaterdagavond voor de activiteit een mailtje ontvangen waarin je kan zien in welk groepje jouw kind zit. Op de activiteit zullen we aanduiden waar de verschillende groepjes moeten gaan staan (aan de hand van nummertjes).</p>
+	
 
 	<h2>Inschrijvings formulier</h2>
 	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfc9RDny7gtiSTxiiQ2ZYwt-ysKKA8KLD5SObOTNbYTK9SHQ/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">Laden ...</iframe>
